f7594642316155b42ec2dc2c9b3436aa928a40db
Tutorials-Git <kimmony039@gmail.com>
2023-08-03 16:09:14 +0700

    delete javascript.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,1 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const menuBtn = document.querySelector(".menu-btn");
-  const navbarItems = document.querySelector(".navbar-items");
-
-  menuBtn.addEventListener("click", () => {
-    navbarItems.classList.toggle("show");
-  });
-});
-
-
-
+document.getElementById('test').innerHTML = "hello world";
